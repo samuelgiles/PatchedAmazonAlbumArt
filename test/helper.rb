413864edb3f4b8a23d8535b8ef4c50cad3077a2b
Samuel Giles <livelikeasam@googlemail.com>
2013-03-13 17:23:29 +0000

    Init

---
+++ test/helper.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'amazon-album-art'
+
+def api_key
+  'api-key'
+end
+def secret_key
+  'secret-key'
+end
+def fixture_file(filename)
+  return '' if filename == ''
+  file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename)
+  File.read(file_path)
+end
